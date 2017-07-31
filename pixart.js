9a8e57d7b59a981d09bd6916ab9bf00ae26a1333
rachel <rache882@gmail.com>
2017-07-31 16:29:24 +1000

    initial commit

--- pixart.js
+++ pixart.js
@@ -3,3 +3,15 @@ $("#set-color").click(function(event) {
   event.preventDefault();
   $(".brush").css("background-color", $("#color-field").val())
 })
+
+$(document).ready(function() {
+  for (var i = 0; i < 20; i++) {
+    var square = $("<div>").addClass("square")
+    $('body').append(square)
+  }
+
+})
+
+// $("body").click(function() {
+//
+// })
